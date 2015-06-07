29c6988091663390005445fdf31a6f6542171cd3
Alexander Ross <alex@aross.se>
2015-06-07 19:48:10 +0200

    Stops create duplicates

--- lib/omnifocus/extrabrain.rb
+++ lib/omnifocus/extrabrain.rb
@@ -1,11 +1,14 @@
 require 'rest-client'
 require 'keychain'
+require 'base64'
+require 'byebug'
 
 module OmniFocus::Extrabrain
   PREFIX = 'EB'
 
   class Task
-    attr_accessor :id, :title, :project_title
+    attr_accessor :id, :title, :project_id, :team
+    attr_writer :project_title
 
     def initialize(attrs = {})
       attrs.each do |attribute, value|
@@ -16,8 +19,12 @@ module OmniFocus::Extrabrain
       end
     end
 
+    def url
+      "https://#{team['subdomain']}.extrabrain.se/tasks/#{id}"
+    end
+
     def ticket_id
-      "#{PREFIX}-#{project_title}##{id}"
+      "#{PREFIX}##{id}"
     end
 
     def project_title
@@ -46,9 +53,8 @@ module OmniFocus::Extrabrain
   def populate_extrabrain_tasks
     teams.each do |team|
       page = 1
-
       while tasks = get_tasks(team, page) do
-        tasks.each { |t| process_extrabrain_task(t, team) }
+        tasks.each { |task| process_extrabrain_task(task, team) }
         page = page + 1
       end
     end
@@ -68,29 +74,29 @@ module OmniFocus::Extrabrain
 
   def user_id
     @user_id ||= begin
-                  current_user = JSON.parse resource['users/current.json'].get
-                  current_user['user']['id']
-                end
+      current_user = JSON.parse resource['users/current.json'].get
+      current_user['user']['id']
+    end
   end
 
   def resource
     @resource ||= client('https://extrabrain.se')
   end
 
-
   def get_tasks(team, page)
     team_resource = client("https://#{team['subdomain']}.extrabrain.se")
     json = JSON.parse team_resource["tasks.json?user_id=#{user_id}&page=#{page}"].get
     return nil if json.nil? || json.empty?
-    json.map { |task_attributes| Task.new(task_attributes) }
+    json.map { |task_attributes| Task.new(task_attributes.merge team: team) }
   end
 
   def process_extrabrain_task(task, team)
-    description = "https://#{team['subdomain']}.extrabrain.se/tasks/#{task.id}"
     if existing[task.ticket_id]
-      bug_db[task.project_title][task.ticket_id] = true
-      return
+      bug_db[existing[task.ticket_id]][task.ticket_id] = true
+    else
+      title = "#{task.ticket_id}: #{task.title}"
+      description = task.url
+      bug_db[task.project_title][task.ticket_id] = [title, description]
     end
-    bug_db[task.project_title][task.ticket_id] = [task.title, description]
   end
 end
